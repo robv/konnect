892faaad236fb56bc2631792fa66689e8374c9cc
robv <robert@pseudolab.com>
2009-05-08 11:46:11 -0500

    Separated helpers and libraries

--- core/master.inc.php
+++ core/master.inc.php
@@ -8,13 +8,13 @@
     // Class Autoloader
     function __autoload($class_name)
     {
-     	$folders = array('core','helpers','config');
+     	$folders = array('core','helpers','config','libraries');
 		foreach($folders as $folder)
 		{
 			if(file_exists(DOC_ROOT . $folder . '/class.' . strtolower($class_name) . '.php'))
 	 		{
 				require DOC_ROOT .  $folder . '/class.' . strtolower($class_name) . '.php';
-				return true;
+				break;
 			}
 		}
     }
