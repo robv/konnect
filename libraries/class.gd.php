befc8f0565fdc236f9bcc2fc2c04982f72f34856
robv <robert@pseudolab.com>
2009-05-10 00:01:10 -0500

    Updated to match clink repository

--- libraries/class.gd.php
+++ libraries/class.gd.php
@@ -1,180 +1,206 @@
 <?php
-    // Simple wrapper class for GD
-    class GD
-    {
-        public $im     = null;
-        public $width  = null;
-        public $height = null;
-        public $type   = null;
-        public $mime   = null;
+/*
+	SOURCE : http://github.com/creativelink/clink-tools/tree/master
+	
+	Example Usage:
+	$im = new Gd_Image('foo.jpg');
+	$im->scale_crop(800, 600); // this method will resize and crop from center, use scale() to just resize to max width and height
+	$im->save_as('foo_new.jpg', 'jpg');
 
-        public function __construct($data = null, $ext = null)
-        {
-            if (is_resource($data) && get_resource_type($data) == 'gd')
-                return $this->loadResource($data);
-            elseif (@file_exists($data) && is_readable($data))
-                return $this->loadFile($data);
-            elseif (is_string($data))
-                return $this->loadString($data);
-            else
-                return false;
-        }
+	TODO Proper Error Handling
+*/
 
-        private function loadResource($im)
-        {
-            if (!is_resource($im) || !get_resource_type($im) == 'gd') return false;
+	class Gd
+	{
+		private $im     = null;
+		private $width  = null;
+		private $height = null;
+		private $type   = null;
 
-            $this->im     = $im;
-            $this->width  = imagesx($im);
-            $this->height = imagesy($im);
+		public function __construct($data = null)
+		{
+			if (is_resource($data) && get_resource_type($data) == 'gd')
+				return $this->load_resource($data);
+			elseif (file_exists($data) && is_readable($data) && is_file($data))
+				return $this->load_file($data);
+			else
+				return false;
+		}
+	
+		public function __destruct()
+		{
+			if (is_resource($this->im) && get_resource_type($this->im) == 'gd')
+			{
+				imagedestroy($this->im);
+			}
+		}
 
-            return true;
-        }
+	    // Return image data as a string.
+	    // Is there a way to do this without using output buffering?
+	    public function __tostring($type = 'jpg', $quality = 75)
+	    {
+	        ob_start();
 
-        private function loadFile($filename)
-        {
-            if (!file_exists($filename) || !is_readable($filename)) return false;
+	        if ($type == 'jpg' && (imagetypesypes() & IMG_JPG))
+	            imagejpeg($this->im, null, $quality);
+	        elseif ($type == 'png' && (imagetypes() & IMG_PNG))
+	            imagepng($this->im);
+	        elseif ($type == 'gif' && (imagetypes() & IMG_GIF))
+	            imagegif ($this->im);
 
-            $info = getimagesize($filename);
-            $this->width  = $info[0];
-            $this->height = $info[1];
-            $this->type   = image_type_to_extension($info[2], false);
-            $this->mime   = $info['mime'];
+	        return ob_get_clean();
+	    }
 
-            if ($this->type == 'jpeg' && (imagetypes() & IMG_JPG))
-                $this->im = imagecreatefromjpeg($filename);
-            elseif ($this->type == 'png' && (imagetypes() & IMG_PNG))
-                $this->im = imagecreatefrompng($filename);
-            elseif ($this->type == 'gif' && (imagetypes() & IMG_GIF))
-                $this->im = imagecreatefromgif ($filename);
-            else
-                return false;
+		private function load_resource($im)
+		{	
+			if (!is_resource($im) || !get_resource_type($im) == 'gd')
+				return false;
 
-            return true;
-        }
+			$this->im     = $im;
+			$this->width  = imagesx($im);
+			$this->height = imagesy($im);
 
-        private function loadString($str)
-        {
-            $im = imagecreatefromstring($str);
-            return ($im === false) ? false : $this->loadResource($im);
-        }
+			return true;
+		}
+	
+		private function load_file($filename)
+		{
+			if (!file_exists($filename) || !is_readable($filename))
+				return false;
 
-        public function saveAs($filename, $type = 'jpg', $quality = 75)
-        {
-            if ($type == 'jpg' && (imagetypes() & IMG_JPG))
-                return imagejpeg($this->im, $filename, $quality);
-            elseif ($type == 'png' && (imagetypes() & IMG_PNG))
-                return imagepng($this->im, $filename);
-            elseif ($type == 'gif' && (imagetypes() & IMG_GIF))
-                return imagegif ($this->im, $filename);
-            else
-                return false;
-        }
+			$info = @getimagesize($filename);
+			if (!$info)
+				return false;
 
-        // Output file to browser
-        public function output($type = 'jpg', $quality = 75)
-        {
-            if ($type == 'jpg' && (imagetypes() & IMG_JPG))
-            {
-                header("Content-Type: image/jpeg");
-                imagejpeg($this->im, null, $quality);
-                return true;
-            }
-            elseif ($type == 'png' && (imagetypes() & IMG_PNG))
-            {
-                header("Content-Type: image/png");
-                imagepng($this->im);
-                return true;
-            }
-            elseif ($type == 'gif' && (imagetypes() & IMG_GIF))
-            {
-                header("Content-Type: image/gif");
-                imagegif ($this->im);
-                return true;
-            }
-            else
-                return false;
-        }
+			$this->width  = $info[0];
+			$this->height = $info[1];
+			$this->type   = image_type_to_extension($info[2], false);
+		
+			if ($this->type == 'jpeg' && (imagetypes() & IMG_JPG))
+			{
+				$im = imagecreatefromjpeg($filename);
+			}
+			elseif ($this->type == 'png' && (imagetypes() & IMG_PNG))
+			{
+				$im = imagecreatefrompng($filename);
+			}
+			elseif ($this->type == 'gif' && (imagetypes() & IMG_GIF))
+			{
+				$im = imagecreatefromgif ($filename);
+			}
+			else
+			{
+				return false;
+			}
 
-        // Return image data as a string.
-        // Is there a way to do this without using output buffering?
-        public function __tostring($type = 'jpg', $quality = 75)
-        {
-            ob_start();
+			return $this->load_resource($im);
+		}
+	
+		public function save_as($filename, $type = 'jpg', $quality = 90)
+		{
+			if ($type == 'jpg' && (imagetypes() & IMG_JPG))
+			{
+				return imagejpeg($this->im, $filename, $quality);
+			}
+			elseif ($type == 'png' && (imagetypes() & IMG_PNG))
+			{
+				return imagepng($this->im, $filename);
+			}
+			elseif ($type == 'gif' && (imagetypes() & IMG_GIF))
+			{
+				return imagegif ($this->im, $filename);
+			}
+			else
+			{
+				return false;
+			}
+		}
 
-            if ($type == 'jpg' && (imagetypes() & IMG_JPG))
-                imagejpeg($this->im, null, $quality);
-            elseif ($type == 'png' && (imagetypes() & IMG_PNG))
-                imagepng($this->im);
-            elseif ($type == 'gif' && (imagetypes() & IMG_GIF))
-                imagegif ($this->im);
+		public function output($type = 'jpg', $quality = 90)
+		{
+			if ($type == 'jpg' && (imagetypes() & IMG_JPG))
+			{
+				header('Content-Type: image/jpeg');
+				imagejpeg($this->im, null, $quality);
+				return true;
+			}
+			elseif ($type == 'png' && (imagetypes() & IMG_PNG))
+			{
+				header('Content-Type: image/png');
+				imagepng($this->im);
+				return true;
+			}
+			elseif ($type == 'gif' && (imagetypes() & IMG_GIF))
+			{
+				header('Content-Type: image/gif');
+				imagegif ($this->im);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 
-            return ob_get_clean();
-        }
+		public function scale($new_width = null, $new_height = null)
+		{
+			if ($new_width <= 0 || $new_height <= 0)
+			{
+				return false;				
+			}
 
-        // Resizes an image and maintains aspect ratio.
-        public function scale($new_width = null, $new_height = null)
-        {
-            if (!is_null($new_width) && is_null($new_height))
-                $new_height = $new_width * $this->height / $this->width;
-            elseif (is_null($new_width) && !is_null($new_height))
-                $new_width = $this->width / $this->height * $new_height;
-            elseif (!is_null($new_width) && !is_null($new_height))
-            {
-                if ($this->width < $this->height)
-                    $new_width = $this->width / $this->height * $new_height;
-                else
-                    $new_height = $new_width * $this->height / $this->width;
-            }
-            else
-                return false;
+			$ratio = $this->width / $this->height;
+			if (($new_width / $new_height) > $ratio)
+			{
+				$new_width = $new_height * $ratio;
+			}
+			else
+			{
+				$new_height = $new_width / $ratio;
+			}
 
-            return $this->resize($new_width, $new_height);
-        }
+			return $this->resize($new_width, $new_height);
+		}
+	
+		public function scale_crop($new_width = null, $new_height = null)
+		{
+			if ($new_width <= 0 || $new_height <= 0)
+				return false;
 
-        // Resizes an image to an exact size
-        public function resize($new_width, $new_height)
-        {
-            $dest = imagecreatetruecolor($new_width, $new_height);
+			$scale = min($this->width / $new_width, $this->height / $new_height);
 
-            // Transparency fix contributed by Google Code user 'desfrenes'
-            imagealphablending($dest, false);
-            imagesavealpha($dest, true);
+			$dst_w = $this->width / $scale;
+			$dst_h = $this->height / $scale;
 
-            if (imagecopyresampled($dest, $this->im, 0, 0, 0, 0, $new_width, $new_height, $this->width, $this->height))
-            {
-                $this->im = $dest;
-                $this->width = imagesx($this->im);
-                $this->height = imagesy($this->im);
-                return true;
-            }
+			$dst_x = ($new_width - $dst_w) / 2;
+			$dst_y = ($new_height - $dst_h) / 2;
 
-            return false;
-        }
+			return $this->resize($new_width, $new_height, $dst_x, $dst_y, $dst_w, $dst_h);
+		}
 
-        public function crop($x, $y, $w, $h)
-        {
-            $dest = imagecreatetruecolor($w, $h);
+		private function resize($new_width, $new_height, $dst_x = 0, $dst_y = 0, $dst_w = 0, $dst_h = 0)
+		{
+			$dest = imagecreatetruecolor($new_width, $new_height);
+		
+			if ($dst_w <= 0 || $dst_h <= 0)
+			{
+				$dst_w = $new_width;
+				$dst_y = $new_height;
+			}
 
-            if (imagecopyresampled($dest, $this->im, 0, 0, $x, $y, $w, $h, $w, $h))
-            {
-                $this->im = $dest;
-                $this->width = $w;
-                $this->height = $h;
-                return true;
-            }
+			// for transparancy
+			imagealphablending($dest, false);  
+			imagesavealpha($dest, true);
 
-            return false;
-        }
+			if (imagecopyresampled($dest, $this->im, $dst_x, $dst_y, 0, 0, $dst_w, $dst_h, $this->width, $this->height))
+			{
+				$this->im = $dest;
+				$this->width = imagesx($this->im);
+				$this->height = imagesy($this->im);
+				return true;
+			}
 
-        public function cropCentered($w, $h)
-        {
-            $cx = $this->width / 2;
-            $cy = $this->height / 2;
-            $x = $cx - $w / 2;
-            $y = $cy - $h / 2;
-            if ($x < 0) $x = 0;
-            if ($y < 0) $y = 0;
-            return $this->crop($x, $y, $w, $h);
-        }
-    }
+			return false;
+		}
+
+	}
