67f72c9131f403f55fc9d9a8d39e41a31d44335e
robv <robert@pseudolab.com>
2009-05-12 15:36:27 -0500

    Added some test cases

---
+++ tests/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Determine our absolute document root, includes trailing slash
+define('DOC_ROOT', realpath(dirname(__FILE__) . '/../') . '/');
+define('KONNECT','poop');
+
+include DOC_ROOT . 'tests/autoload.php';
+
+// START TEST ///////
+
+include DOC_ROOT . 'core/class.config.php';
+
+
+	
+	Config::set_config('konnect.dev');
+	
+	var_dump(Config::$core);
