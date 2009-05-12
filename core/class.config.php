9a894c49cb5c389612858423bedf92308fd504ff
robv <robert@pseudolab.com>
2009-05-12 15:36:16 -0500

    Terminix

--- core/class.config.php
+++ core/class.config.php
@@ -13,72 +13,62 @@
         private static $me;
 		
         // The basics
-        public $auth; // Array for all auth information
-        public $db; // Array for all db information
-		public $core; // Array for core information
-		public $routes; // Core routes
+        public static $auth; // Array for all auth information
+        public static $db; // Array for all db information
+		public static $core; // Array for core information
+		public static $routes; // Core routes
+		public static $servers; // Core routes
 		
 		// App info
-        public $default_app; // Default app to direct to
-		public $installed_apps;
+        public static $default_app; // Default app to direct to
+		public static $installed_apps;
 
         // Singleton constructor
         private function __construct()
         {
-           	$this->everywhere();
-           	$this->set_config();
+           	self::everywhere();
+           	self::set_config();
         }
 
-        // Get Singleton object
-        public static function exec()
-        {
-            if (is_null(self::$me))
-                self::$me = new Config();
-            return self::$me;
-        }
-
-        // Allow access to config settings statically.
-        // Ex: Config::get('some_value')
-        public static function get($key)
-        {
-            return self::$me->$key;
-        }
-
-		public function set_config_vars($config = array()) 
+		public static function set_config_vars($config = array()) 
 		{
 			foreach ($config as $k => $v)
-				if(isset($this->$k) || is_null($this->$k))
-					$this->$k = $v;
+				if(isset(self::$$k) || is_null(self::$$k))
+					self::$$k = $v;
 		}
 
         // Add code to be run on all servers
-        private function everywhere()
+        private static function everywhere()
         {
             // Store sesions in the database?
-            $this->db['session'] = TRUE;
+            self::$db['session'] = TRUE;
 
             // Settings for the Auth class
-            $this->auth['domain'] = $_SERVER['HTTP_HOST'];
-            $this->auth['hash'] = TRUE;
-            $this->auth['salt'] = 'wtnMmVyc8vhkrxBrtkm3VTkLwiAFs'; // Pick any random string of characters
+            self::$auth['domain'] = $_SERVER['HTTP_HOST'];
+            self::$auth['hash'] = TRUE;
+            self::$auth['salt'] = 'wtnMmVyc8vhkrxBrtkm3VTkLwiAFs'; // Pick any random string of characters
 
         }
 
-        public function set_config()
+        public static function set_config($host = NULL)
         {
+			// Allows testing outside of browser by being able to pass host
+			if(is_null($host))
+				$host = $_SERVER['HTTP_HOST'];
+	
 			// Returns the array $config and also $core so that we don't have to define all those settings here
 			include DOC_ROOT . 'config/settings.php';
-			
+
 			// Load $core settings into object
-			$this->set_config_vars($core);
+			self::set_config_vars($core);
 			
 			foreach ($config as $name => $settings)
 			{
 				// Search server array to see if where we are matches, if true, then we know what settings to use
-	            if (in_array($_SERVER['HTTP_HOST'], $settings['servers']))
+	            if (in_array($host, $settings['servers']))
 				{
-					$this->set_config_vars($settings);
-		            define('WEB_ROOT', $this->core['web_root']);
+					self::set_config_vars($settings);
+		            define('WEB_ROOT', self::$core['web_root']);
 				}
 				if (!defined('WEB_ROOT')) {
 					die('<h1>Where am I?</h1> <p>You need to setup your server names in <code>settings.php</code></p>
