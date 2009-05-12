6d604fd774bda032ad98d690f64ac580185fc224
section31 <dave@creativelink.com>
2009-05-12 14:15:02 -0500

    change uri to satic property

--- core/class.router.php
+++ core/class.router.php
@@ -11,29 +11,21 @@ class Router {
 	// Singleton object. Leave $me alone.
 	private static $me;
 
-	public $uri;
+	public static $uri;
    
-   	// Get Singleton object
-       public static function exec()
-       {
-           if (is_null(self::$me))
-               self::$me = new Router();
-           return self::$me;
-       }
-	
 	// Simply runs both uri_to_array and uri_rewrite so we don't have to run both methods
 	public static function new_uri($routes, $uri = NULL)
 	{
-		$this->uri = $this->uri_to_array($uri);
-		$this->uri = $this->uri_rewrite($routes);
-		return $this->uri;
+		Router::$uri = $this->uri_to_array($uri);
+		Router::$uri = $this->uri_rewrite($routes);
+		return Router::$uri;
 	}
 	
 	// Maps current uri array to see if matches are found in config/settings.php
 	public static function uri_rewrite($routes)
 	{
 			
-		$uri_string = implode('/',$this->uri) . '/';
+		$uri_string = implode('/', Router::$uri) . '/';
 		$matches = array();
 	
 		foreach ($routes as $intial_path => $destination_path)
@@ -43,14 +35,14 @@ class Router {
 				foreach ($matches as $key => $value)
 				{ 
 					// in destination path use %1%, %2%, etc as you would $1, $2, in mod_rewrite
-					$destination_path = str_replace('%'.$key.'%',$value,$destination_path);
+					$destination_path = str_replace('%' . $key . '%',$value,$destination_path);
 				}
 				// triming shouldn't be neccessary it's done just in case
-				return $this->uri = explode('/',trim(strtolower($destination_path),'/')); 
+				return Router::$uri = explode('/', trim(strtolower($destination_path), '/')); 
 			}
 		}
 		
-		return $this->uri;
+		return Router::$uri;
 	}
 
 	public static function uri_to_array($uri = NULL)
@@ -60,24 +52,23 @@ class Router {
 			$uri = $_SERVER['REQUEST_URI'];
 		
 		// Lowercase the entire string then strip http, https and ftp (just for fun) our of uri and then explode by "/"
-		$this->uri = explode('/', trim(str_replace(array('http://','https://','ftp://'), '', strtolower($uri)), '/'));
+		Router::$uri = explode('/', trim(str_replace(array('http://','https://','ftp://'), '', strtolower($uri)), '/'));
 	
-		$new_uri = array();
-		
 		// We want to rebuild the array without the actual domain
 		if (@parse_url($uri))
-			array_shift($this->uri);
-			
-		foreach ($this->uri as $key => $singleton)
+			array_shift(Router::$uri);
+
+		$new_uri = array();			
+		foreach (Router::$uri as $key => $singleton)
 		{	
 			// String the string of all special characters
-			$new_uri[] = String::exec()->clean($singleton,'-');
+			$new_uri[] = String::clean($singleton, '-');
 		}
 	
 		// Replace uri with the new modified version
-		$this->uri = $new_uri;
+		Router::$uri = $new_uri;
 	
-		return $this->uri;
+		return Router::$uri;
     }
 
 }
