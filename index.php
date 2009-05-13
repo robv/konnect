43467eff1bbcb8bb0e8e35dd9ddb9d367036df12
robv <robert@pseudolab.com>
2009-05-13 11:34:23 -0500

    Index now using static methods

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 	
 	require 'core/master.inc.php';
 	
-	Router::exec()->new_uri(Config::exec()->routes);
+	Router::new_uri(Config::$config['core']['routes']);
 	
 	// If no app is set through the uri array then set it to default
-	if (!isset(Router::exec()->uri['0']) || empty(Router::exec()->uri['0']))
-		Router::exec()->uri['0'] = Config::exec()->default_app;
+	if (!isset(Router::$uri['0']) || empty(Router::$uri['0']))
+		Router::$uri['0'] = Config::$config['core']['default_app'];
 	
 	// If app doesn't exist then show error
-	if (!in_array(Router::exec()->uri['0'], Config::exec()->installed_apps))
-		die ('<h1>Opps</h1> <p>The app <strong>' . Router::exec()->uri['0'] . '</strong> does not exist.</p>');
+	if (!in_array(Router::$uri['0'], Config::$config['core']['installed_apps']))
+		die ('<h1>Opps</h1> <p>The app <strong>' . Router::$uri['0'] . '</strong> does not exist.</p>');
 		
 	// Import  init class for app, should always be apps/appname/init.php
-	require DOC_ROOT . 'apps/' . Router::exec()->uri['0'] . '/init.php';
+	require DOC_ROOT . 'apps/' . Router::$uri['0'] . '/init.php';
 	
 	// Build init class name, should always be ucwords of app name followed by _init, example this_admin = This_Admin_init
-	$init_class = String::exec()->uc_slug(Router::exec()->uri['0'], '_') . '_init'; 
+	$init_class = String::uc_slug(Router::$uri['0'], '_') . '_init'; 
 	
-	$init_class_obj = new $init_class(Router::exec()->uri['0']); // initiate init class
+	$init_class_obj = new $init_class(Router::$uri['0']); // initiate init class
