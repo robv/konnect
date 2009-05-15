fc47c61dcf5ec373fd90507b03b6b6d29f9272b0
section31 <dave@creativelink.com>
2009-05-15 16:41:04 -0500

    Updating Indents and Coding Standard.

--- core/master.inc.php
+++ core/master.inc.php
@@ -1,44 +1,41 @@
 <?php
-    // Application flag
-    define('KONNECT', true);
 
-    // Determine our absolute document root, includes trailing slash
-    define('DOC_ROOT', realpath(dirname(__FILE__) . '/../') . '/');
-	
-	include DOC_ROOT . 'core/class.config.php';
-	
-    // Class Autoloader
-    function __autoload($class_name)
-    {
-		$folders = array();
-	
-		foreach(Config::$config['core']['installed_apps'] as $app)
-		{
-			$folders[] = 'apps/' . $app . '/libraries';
+// Application flag
+define('KONNECT', true);
+
+// Determine our absolute document root, includes trailing slash
+define('DOC_ROOT', realpath(dirname(__FILE__) . '/../') . '/');
+
+include DOC_ROOT . 'core/class.config.php';
+
+// Class Autoloader
+function __autoload($class_name)
+{
+	$folders = array();
+
+	foreach(Config::$config['core']['installed_apps'] as $app) {
+		$folders[] = 'apps/' . $app . '/libraries';
+	}
+
+	$folders = array_merge($folders, array('core', 'helpers', 'libraries'));
+	foreach ($folders as $folder) {
+		if (file_exists(DOC_ROOT . $folder . '/class.' . strtolower($class_name) . '.php')) {
+			require DOC_ROOT .  $folder . '/class.' . strtolower($class_name) . '.php';
+			break;
 		}
+	}
+}
 
-     	$folders = array_merge($folders, array('core', 'helpers', 'libraries'));
-		foreach ($folders as $folder)
-		{
-			if (file_exists(DOC_ROOT . $folder . '/class.' . strtolower($class_name) . '.php'))
-	 		{
-				require DOC_ROOT .  $folder . '/class.' . strtolower($class_name) . '.php';
-				break;
-			}
-		}
-    }
+// Global include files
+require DOC_ROOT . 'core/class.db_object.php'; // TODO: Will this be autoloaded on extends?
 
-    // Global include files
-    require DOC_ROOT . 'core/class.db_object.php'; // TODO: Will this be autoloaded on extends?
+// Fix magic quotes
+if (get_magic_quotes_gpc()) {
+	$_POST    = String::strip_slashes($_POST);
+	$_GET     = String::strip_slashes($_GET);
+	$_REQUEST = String::strip_slashes($_REQUEST);
+	$_COOKIE  = String::strip_slashes($_COOKIE);
+}
 
-    // Fix magic quotes
-    if (get_magic_quotes_gpc())
-    {
-        $_POST    = String::strip_slashes($_POST);
-        $_GET     = String::strip_slashes($_GET);
-        $_REQUEST = String::strip_slashes($_REQUEST);
-        $_COOKIE  = String::strip_slashes($_COOKIE);
-    }
-
-    // Initialize our session
-    session_start();
+// Initialize our session
+session_start();
