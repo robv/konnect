a95fe8dcc5cfb2ba4b6bcb4e0692f5babca559b1
robv <thisisrobv@gmail.com>
2009-08-19 18:30:19 -0500

    Modified / cleaned up css files.

--- js/plugins/jquery.inputhint.js
+++ js/plugins/jquery.inputhint.js
@@ -1,17 +1,22 @@
 jQuery.fn.input_hint = function() {
+	
+    return this.each(function() {
+	
+        var $this = $(this);
 
-	return this.each(function() {
-		var $this = $(this);
-		
-		$this.attr('value', $(this).attr('title'));
-	
-		$this.focus(function() {
-			if ($(this).attr('value') == $(this).attr('title'))
-				$(this).attr('value', '');
-		}).blur(function() {
-			if ($(this).attr('value') == '')
-				$(this).attr('value', $(this).attr('title'));
-		});
-	});
-	
-}
+        $this.attr('value', $this.attr('title'));
+
+        $this.focus(function() {
+            if ($this.attr('value') == $this.attr('title'))
+                $this.attr('value', '');
+        }).blur(function() {
+            if ($this.attr('value') == '')
+                $this.attr('value', $this.attr('title'));
+        }).parents('form').submit(function() {
+            if ($this.attr('value') == $this.attr('title'))
+                $this.attr('value', '');
+        });
+
+    });
+
+};
